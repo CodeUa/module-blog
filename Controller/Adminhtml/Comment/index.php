dbe9d0adeafc76cf2499f00cc6bb74b4a8a9e623
Andriy <martynovskyj_aa@fizmat.tnpu.edu.ua>
2017-05-08 17:05:07 +0300

    add admin page

---
+++ Controller/Adminhtml/Comment/index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Magefan\Blog\Controller\Adminhtml\Comment;
+
+class Index extends \Magento\Backend\App\Action
+{
+	public function execute(){
+		die("hello Webkul Employee");
+	}
+
+}
